e5df3b0ab7f4f29667c031b16785eef9d14d5d32
Felipe Zanon <sharpion.k@gmail.com>
2018-12-02 01:21:17 -0200

    Added jQuery + field validation + onkeypress submit.

--- js/main.js
+++ js/main.js
@@ -1,15 +1,36 @@
-function login() {
-    let username = document.getElementById('username').value;
-    let password = document.getElementById('password').value;
-    
-//    Correct username and password are explicit here as requested for this test.
-    if (username === "test" && password === "123") {
-        const loginBox = document.getElementById("login-box");
-        loginBox.classList.add("hidden");
-        
-        const welcomeScreen = document.getElementById("welcome-screen");
-        welcomeScreen.classList.remove("hidden");
+$(document).on("keyup",function(e){
+
+    let username = $("#username").val();
+    let password = $("#password").val();
+
+    if (!username || !password) {
+        $("#button").prop('disabled', true);
+        return;
     }
-    else
-        alert("WRONG");
+
+    $("#button").prop('disabled', false);
+
+    if(e.key === "Enter")
+        login();
+
+});
+
+
+function login() {
+    let username = $("#username").val();
+    let password = $("#password").val();
+    
+    // Correct username and password are explicit here as requested for this test.
+    if (username === "test" && password === "123") {
+        $("#login-box").addClass("hidden");
+        $("#welcome-screen").removeClass("hidden");
+    }
+    else {
+        $("#login-box").addClass("shake");
+        setTimeout(function () {
+            $('#login-box').removeClass("shake");
+        }, 1000);
+
+    }
+
 }
